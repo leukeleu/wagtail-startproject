bc6c5d2d9798cc9adea6b518e02d852289e47cdb
cho-leukeleu <cho@leukeleu.nl>
2017-08-15 17:19:30 +0200

    Update settings_default.py
    To avoid funny errors while running selenium tests like
    
    IntegrityError: Problem installing fixtures: insert or update on table "wagtailcore_page" violates foreign key constraint "wagtailcore__content_type_id_c28424df_fk_django_content_type_id"

--- wagtailstartproject/project_template/project_name/settings_default.py
+++ wagtailstartproject/project_template/project_name/settings_default.py
@@ -45,9 +45,9 @@ INSTALLED_APPS = [
     'modelcluster',
     'taggit',
 
+    'django.contrib.contenttypes',
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
