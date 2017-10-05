b86d135bb1e5d160316886838f91b95f90d5e86f
Ramon de Jezus <rdejezus@leukeleu.nl>
2017-10-05 14:58:53 +0200

    Bump version to 0.1.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='wagtailstartproject',
-    version='0.1.4',
+    version='0.1.5',
     description='This app contains a custom project template and a custom wagtail start command to allow creating customized wagtail setups',
     long_description=io.open('README.rst', encoding='utf-8').read(),
     keywords=['wagtail', 'startproject'],
