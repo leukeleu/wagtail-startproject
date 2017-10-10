4d1455614beea6b751715fbd0a0547bbe3dea018
Ramon de Jezus <rdejezus@leukeleu.nl>
2017-10-10 09:58:28 +0200

    Update PageStatusMiddleware for use with Python 3

--- wagtailstartproject/project_template/tests/middleware.py
+++ wagtailstartproject/project_template/tests/middleware.py
@@ -16,8 +16,8 @@ class PageStatusMiddleware(MiddlewareMixin):
         # (expecting response['Content-Type'] to be ~ 'text/html; charset=utf-8')
         if 'html' in response['Content-Type'].lower():
             response.content = response.content.replace(
-                '</head>',
-                '<meta name="status_code" content="{status_code}" /></head>'.format(status_code=response.status_code),
+                b'</head>',
+                bytes('<meta name="status_code" content="{status_code}" /></head>'.format(status_code=response.status_code), encoding='ascii'),
                 1
             )
 
